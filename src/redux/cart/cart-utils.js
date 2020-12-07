9e30d7670f31cb510f3385295bc53972dd1a14a3
bartek <bartek.sarzalski@gmail.com>
2020-12-07 08:37:29 +0100

    updated shopdata

--- src/redux/cart/cart-utils.js
+++ src/redux/cart/cart-utils.js
@@ -10,5 +10,6 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
             : cartItem
         );
     }
-    return [...cartItems, { ...cartItemsToAdd, quantity: 1 }];
+    
+    return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
