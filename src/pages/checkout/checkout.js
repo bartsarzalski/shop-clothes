1eebe8e26a9107a5f68bf18e5118436fa447acbe
bartek <bartek.sarzalski@gmail.com>
2020-12-16 09:30:05 +0100

    adding remove button

--- src/pages/checkout/checkout.js
+++ src/pages/checkout/checkout.js
@@ -34,7 +34,7 @@ const CheckoutPage = ({ cartItems, total }) => (
                 <CheckoutItem key={cartItem.id} cartItem={cartItem} />)
         }
         <div className="total">
-            <span>Total: ${total}</span>
+            <span>Total: {total}$ </span>
         </div>
     </div>
 );
