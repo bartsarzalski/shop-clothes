00d24b460fd7cfc20e5f833dcd0492506d3a128a
bartek <bartek.sarzalski@gmail.com>
2020-12-20 12:42:02 +0100

    adding remove functionality to checkout-item component

--- src/redux/cart/cart-types.js
+++ src/redux/cart/cart-types.js
@@ -3,7 +3,8 @@ import CartDropdown from "../../components/cart-dropdown/cart-dropdown";
 const CartActionTypes = {
     TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN',
     ADD_ITEM: 'ADD_ITEM',
-    CLEAR_ITEM_FROM_CART: 'CLEAR_ITEM_FROM_CART'
+    REMOVE_ITEM: 'REMOVE_ITEM',
+    DELETE_ONE_ITEM_FROM_CART: 'DELETE_ONE_ITEM_FROM_CART'
 };
 
 export default CartActionTypes;
